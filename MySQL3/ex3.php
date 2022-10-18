c5cdc9c32d3602474c52ec77cf629439c5036fb1
Darius Kliminskas <d.kliminskas@talentator.com>
2022-10-18 21:51:24 +0300

    Ex3 completed

---
+++ MySQL3/ex3.php
@@ -0,0 +1,20 @@
+<?php
+
+require './connection.php';
+
+$country = "United States";
+
+$a = "SELECT c.name, ci.name
+FROM country AS c 
+LEFT JOIN city AS ci ON c.id = ci.country_id 
+WHERE c.name = :Cname
+AND ci.name like '%on%'";
+
+$statement = $connection->prepare($a);
+$statement->execute(['Cname'=>$country]);
+$data = $statement->fetchAll(\PDO::FETCH_BOTH);
+echo $data[0][0] . PHP_EOL;
+foreach ($data as $value) {
+    echo $value['name'] . PHP_EOL;
+}
+//print_r($data);
